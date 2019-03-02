a900c369ceab7e2f1d9b940ddfc665051109f5fa
Vishal Bakshi <vdbakshi@gmail.com>
2019-03-01 16:27:57 -0800

    Add state to PersonalFinanceVisualization.js

--- components/PersonalFinanceVisualization.js
+++ components/PersonalFinanceVisualization.js
@@ -4,6 +4,13 @@ import GrossMonthlyIncomeCalculator from "./GrossMonthlyIncomeCalculator";
 import ExpenseTable from "./ExpenseTable";
 
 class PersonalFinanceVisualization extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expenses: [],
+      multiplier: 1
+    };
+  }
   render() {
     return (
       <div>
